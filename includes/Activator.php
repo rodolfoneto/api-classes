1180eebcfd4fd6df704fe6d93bf18fff426ed3e3
Rodolfo Neto <rodolfoneto@gmail.com>
2021-02-05 12:09:26 -0300

    First Code

---
+++ includes/Activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       http://rodolfoneto.com.br
+ * @since      1.0.0
+ *
+ * @package    Api_Classes
+ * @subpackage Api_Classes/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Api_Classes
+ * @subpackage Api_Classes/includes
+ * @author     Rodolfo Neto <rodolfoneto@gmail.com>
+ */
+class Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/Deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://rodolfoneto.com.br
+ * @since      1.0.0
+ *
+ * @package    Api_Classes
+ * @subpackage Api_Classes/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Api_Classes
+ * @subpackage Api_Classes/includes
+ * @author     Rodolfo Neto <rodolfoneto@gmail.com>
+ */
+class Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
